27eb23bc0ed1e2fc42b8754cfe32b59c07fc277f
Matías David Schwerdt <schwerdt.matias@gmail.com>
2019-09-17 21:49:04 -0300

    .

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 
 
 int main(){
-    tLista * l = NULL;
-    crear_lista(l);
-    printf("La lista debe tener 0 elementos y tiene: %i/n",l_longitud(*l));
+    tLista l;
+    crear_lista(&l);
+  /*  printf("La lista debe tener 0 elementos y tiene: %i/n",l_longitud(l));
 
-//    int i;
-    //for(i=1; i<=10 ;i++)
-      //  l_insertar(*l, l_ultima(*l), i);
+    int i;
+    for(i=1; i<=10 ;i++)
+        l_insertar(l, l_ultima(l), i);
 
-    printf("La lista debe tener 10 elementos y tiene: %i/n",l_longitud(*l));
-   // printf("El primer elemento debe ser 1 y es: %i/n",(l_primera(*l)->elemento));
-//    printf("El ultimo elemento debe ser 10 y es %i/n"),l_fin(l)->elemento;
-
+    printf("La lista debe tener 10 elementos y tiene: %i/n",l_longitud(l));
+    printf("El primer elemento debe ser 1 y es: %i/n",l_primera(l)->elemento);
+    printf("El ultimo elemento debe ser 10 y es %i/n"),l_fin(l)->elemento;
+*/
 
 }
