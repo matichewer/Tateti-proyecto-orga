195dada5edc444ab88ba3bc08e2a8289249edd72
matichewer <schwerdt.matias@gmail.com>
2019-10-27 14:40:30 -0300

    iniciando la GUI

---
+++ GUI.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+
+#include "partida.h"
+
+
+// Barra \ para compu rota que no le anda ese botón
+
+
+
+void solicitarNombres(){
+    printf("Ingrese el nombre del jugador 1:")
+}
+
+
+
+int solicitarModo(){
+
+    int opcion_elegida = -1;
+
+
+    // Imprimo opciones
+    printf("Ingrese el modo de juego:\n\n");
+    printf("  1. Humano vs IA\n");
+    printf("  2. Humano vs Humano\n");
+
+
+    // Solicito opción elegida por el usuario
+    while(opcion_elegida == 1 || opcion_elegida == 2)
+        scanf("%d", &opcion_elegida);
+
+
+    // Parseo opcion
+    if(opcion_elegida == 1)
+        opcion_elegida = PART_MODO_USUARIO_VS_AGENTE_IA;
+    else
+        opcion_elegida = PART_MODO_USUARIO_VS_USUARIO;
+
+
+    return opcion_elegida
+}
+
+
+
+void solicitarQuienEmpieza(){}
+
+
+
+void mostrarTablero(){}
+
+
+
+int main(){
+
+// Valores a iniciar:
+//tPartida * p, int modo_partida, int comienza, char * j1_nombre, char * j2_nombre
+
+    return 0;
+}
