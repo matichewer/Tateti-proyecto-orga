6ed5fcb31ac6058c15c0aebe00fa016d0b9991e1
matichewer <schwerdt.matias@gmail.com>
2019-11-17 18:11:13 -0300

    Update compilar-y-ejecutar-proyecto.sh

--- compilar-y-ejecutar-proyecto.sh
+++ compilar-y-ejecutar-proyecto.sh
@@ -1,4 +1,3 @@
-
 # Compilo todos los archivos fuente
 gcc -Wall -g -c arbol.c -o arbol.o
 gcc -Wall -g -c lista.c -o lista.o
@@ -6,18 +5,14 @@ gcc -Wall -g -c ia.c -o ia.o
 gcc -Wall -g -c main.c -o main.o
 gcc -Wall -g -c partida.c -o partida.o
 
-
 # Genero el ejecutable
 g++ -o Tateti-Ejecutable arbol.o ia.o lista.o main.o partida.o  
-
 
 # Borro los archivos compilados
 rm *.o
 
-
 # Ejecuto
 ./Tateti-Ejecutable
-
 
 # Borro el ejecutable
 rm Tateti-Ejecutable
