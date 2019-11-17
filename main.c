4245a6e1a66c676ffbf520566091fda6c4c62f6d
matichewer <schwerdt.matias@gmail.com>
2019-11-16 21:38:51 -0300

    GUI - Humano vs Humano casi terminado

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "lista.h"
 #include "arbol.h"
 #include "partida.h"
 #include "ia.h"
-
-// Barra \ para compu rota que no le anda ese botón
 
 
 void asignar_modo_de_juego(int * modo){
@@ -19,11 +18,10 @@ void asignar_modo_de_juego(int * modo){
     printf("\n\n");
 
     // Computo el valor
-    if(*modo == 1)
-        *modo = PART_MODO_USUARIO_VS_USUARIO;
+    if((*modo) == 1)
+        (*modo) = PART_MODO_USUARIO_VS_USUARIO;
     else
-        *modo = PART_MODO_USUARIO_VS_AGENTE_IA;
-
+        (*modo) = PART_MODO_USUARIO_VS_AGENTE_IA;
 }
 
 void asignar_nombre_jugador1(char * nombre){
@@ -46,68 +44,53 @@ void asignar_turno(int * turno){
     scanf("%i",turno);
     printf("\n\n");
 
-    if(*turno == 1)
-        *turno = PART_JUGADOR_1;
+    if((*turno) == 1)
+        (*turno) = PART_JUGADOR_1;
     else
-        if(*turno == 2)
-            *turno = PART_JUGADOR_2;
+        if((*turno) == 2)
+            (*turno) = PART_JUGADOR_2;
         else
-            *turno = PART_JUGADOR_RANDOM;
+            (*turno) = PART_JUGADOR_RANDOM;
 }
 
-void mostrar_ficha_de_primer_y_tercer_columna(tTablero tablero, int fila, int columna){
-
-    if(tablero->grilla[fila][columna] == PART_SIN_MOVIMIENTO)
-        printf("    ");
-
-    if(tablero->grilla[fila][columna] == PART_JUGADOR_1)
-        printf("  X  ");
-
-    if(tablero->grilla[fila][columna] == PART_JUGADOR_2)
-        printf("  O  ");
-
-}
-
-void mostrar_ficha_de_columna_central(tTablero tablero, int fila, int columna){
-
-    if(tablero->grilla[fila][columna] == PART_SIN_MOVIMIENTO)
-        printf("|   |");
-
-    if(tablero->grilla[fila][columna] == PART_JUGADOR_1)
-        printf("| X |");
-
-    if(tablero->grilla[fila][columna] == PART_JUGADOR_2)
-        printf("| O |");
-
-}
 
 void mostrar_tablero(tTablero tablero){
-    printf("TABLERO:\n\n");
-    mostrar_ficha_de_primer_y_tercer_columna(tablero, 0, 0);
-    mostrar_ficha_de_columna_central(tablero, 0, 1);
-    mostrar_ficha_de_primer_y_tercer_columna(tablero, 0, 2);
-    printf("\n ---|---|--- \n");
-    mostrar_ficha_de_primer_y_tercer_columna(tablero, 1, 0);
-    mostrar_ficha_de_columna_central(tablero, 1, 1);
-    mostrar_ficha_de_primer_y_tercer_columna(tablero, 1, 2);
-    printf("\n ---|---|--- \n");
-    mostrar_ficha_de_primer_y_tercer_columna(tablero, 2, 0);
-    mostrar_ficha_de_columna_central(tablero, 2, 1);
-    mostrar_ficha_de_primer_y_tercer_columna(tablero, 2, 2);
+    int i, j;
+    char ficha[8];
+    printf("\n-------------\n");
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            if(tablero->grilla[i][j] == PART_JUGADOR_1)
+                strcpy(ficha, "X");
+            else if(tablero->grilla[i][j] == PART_JUGADOR_2)
+                    strcpy(ficha, "O");
+                else
+                    strcpy(ficha, " ");
+            if(j==1)
+                printf(" %s ", ficha);
+            else
+                printf("| %s |", ficha);
+        }
+        printf("\n-------------\n");
+    }
     printf("\n");
 }
 
 
+
 int main(){
 
+    // Declaro variables.
     tPartida partida;
     tTablero tablero;
     char nombreJugador1[50];
     char nombreJugador2[50];
-    int turno;
-    int modo;
+    int turno, modo;
+    int estadoDePartida, opcionValida;
+    int fila, columna;
 
 
+    // Inicializo y reservo memoria para las variables.
     partida = (tPartida) malloc(sizeof(struct partida));
     if(partida == NULL) {
         printf("El juego no pudo ser cargado. Memoria insuficiente.");
@@ -121,35 +104,76 @@ int main(){
     }
 
 
+    // Asigno modo de juego.
     asignar_modo_de_juego(&modo);
 
+
+    // Asigno nombre de jugadores.
     asignar_nombre_jugador1(nombreJugador1);
     if(modo == PART_MODO_USUARIO_VS_USUARIO)
         asignar_nombre_jugador2(nombreJugador2);
-    else{
-        nombreJugador2[0] = 'M';  // por qué letra por letra ????
-        nombreJugador2[1] = 'a';
-        nombreJugador2[2] = 'q';
-        nombreJugador2[3] = 'u';
-        nombreJugador2[4] = 'i';
-        nombreJugador2[5] = 'n';
-        nombreJugador2[6] = 'a';
-    }
+    else
+        strcpy(nombreJugador2, "Maquina");
 
+
+    // Asigno quien empieza.
     asignar_turno(&turno);
 
+
+    // Creo nueva partida.
     nueva_partida(&partida, modo, turno, nombreJugador1, nombreJugador2);
     tablero = partida->tablero;
     mostrar_tablero(tablero);
 
-    /*
-    if(modo == PART_MODO_USUARIO_VS_USUARIO)
-        iniciar_modo_usuario_vs_usuario();    // falta crear e implementar ésta función
-    else
-        if(modo == PART_MODO_USUARIO_VS_AGENTE_IA)
-            iniciar_modo_usuario_vs_ia();  // falta crear e implementar ésta función
-*/
 
+    // Inicio una partida Usuario vs Usuario
+    if(modo == PART_MODO_USUARIO_VS_USUARIO){
+        while(partida->estado == PART_EN_JUEGO){
+
+            if(turno == PART_JUGADOR_1)
+                printf("%s indicá tu movimiento (entre 1 y 3 inclusive).\n", nombreJugador1);
+            else
+                printf("%s indicá tu movimiento (entre 1 y 3 inclusive).\n", nombreJugador2);
+
+            opcionValida = 0;
+            while(!opcionValida){
+                printf("Fila: ");
+                scanf("%i", &fila);
+                printf("Columna: ");
+                scanf("%i", &columna);
+                if(fila<1 || fila>3 || columna<1 || columna>3)
+                    printf("Error: debes elegir una fila y una columna entre 1, 2 y 3.\n");
+                else
+                    opcionValida = 1;
+            }
+
+            estadoDePartida = nuevo_movimiento(partida, fila-1, columna-1);
+            if(estadoDePartida == PART_MOVIMIENTO_ERROR)
+                printf("Error: debes elegir un casillero vacío.\n");
+            mostrar_tablero(tablero);
+            printf("%i", partida->estado == PART_EN_JUEGO);
+        }
+    }
+    // Inicio una partida Usuario vs IA
+    else
+        if(modo == PART_MODO_USUARIO_VS_AGENTE_IA){
+
+            // aca hay que implementar el modo HUMANO vs IA
+
+        }
+
+
+
+
+
+    if(estadoDePartida == PART_GANA_JUGADOR_1)
+        printf("¡¡ Ganó %s!!", nombreJugador1);
+    else if(estadoDePartida == PART_GANA_JUGADOR_2)
+            printf("¡¡ Ganó %s!!",nombreJugador2);
+        else if(estadoDePartida  == PART_EMPATE)
+                printf("La partida terminó en empate");
+
+    finalizar_partida(&partida);
     return 0;
 }
 
