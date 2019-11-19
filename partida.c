3aa48032cb5593a39acf8e167bf0a3ae164e59e2
matichewer <schwerdt.matias@gmail.com>
2019-11-19 11:59:07 -0300

    pequeña mejora en el random, falta seguir

--- partida.c
+++ partida.c
@@ -32,7 +32,7 @@ void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombr
 
     // Modo de juego al azar
     if(comienza == PART_JUGADOR_RANDOM)
-        if(rand()%100 < 50)
+        if(rand()%2==0)
             (*p)->turno_de = PART_JUGADOR_1;
         else
             (*p)->turno_de = PART_JUGADOR_2;
