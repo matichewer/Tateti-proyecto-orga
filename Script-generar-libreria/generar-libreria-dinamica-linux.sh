15203e363a21e03ac2e74bac319ee4bdc2a468e6
matichewer <schwerdt.matias@gmail.com>
2019-11-19 22:39:49 -0300

    acomodé scripts

--- Script-generar-libreria/generar-libreria-dinamica-linux.sh
+++ Script-generar-libreria/generar-libreria-dinamica-linux.sh
@@ -6,17 +6,13 @@ gcc -Wall -c lista.c arbol.c partida.c ia.c main.c -fPIC
 
 
 
-
 # Genero la libreria
 ld -o libtateti.so lista.o arbol.o -shared
-# le tuve que sacar el -Wall
-
 
 
 
 # Genero ejecutable
 gcc -Wall -o Tateti-libreria-dinamica-linux partida.o ia.o main.o -L. -ltateti
-
 
 
 
